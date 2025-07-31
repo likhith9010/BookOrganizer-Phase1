4dc7bfc9b8dfc39192e6df2364a61ffe1837ee07
likhith bade <likhith>
2025-07-31 20:50:12 +0530

    Fix deployment pathing issue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // FIX: This tells Vite to use relative paths for the build.
+  base: './', 
   plugins: [react()],
 })
